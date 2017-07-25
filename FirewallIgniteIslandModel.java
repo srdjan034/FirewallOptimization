54c81d18d781b1ec67b1a64b90e17834eee30444
srdjan034 <snikolic.imi@gmail.com>
2017-07-25 19:20:01 +0200

    Promenjeni parametri varijacije.

--- FirewallIgniteIslandModel.java
+++ FirewallIgniteIslandModel.java
@@ -6,6 +6,7 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Properties;
 import java.util.UUID;
 
 import org.apache.commons.lang3.SystemUtils;
@@ -16,6 +17,7 @@ import org.apache.ignite.lang.IgniteCallable;
 import org.moeaframework.algorithm.NSGAII;
 import org.moeaframework.algorithm.PeriodicAction;
 import org.moeaframework.algorithm.PeriodicAction.FrequencyType;
+import org.moeaframework.analysis.plot.Plot;
 import org.moeaframework.core.NondominatedPopulation;
 import org.moeaframework.core.PRNG;
 import org.moeaframework.core.Problem;
@@ -28,6 +30,7 @@ import org.moeaframework.core.operator.RandomInitialization;
 import org.moeaframework.core.operator.TournamentSelection;
 import org.moeaframework.core.operator.real.PM;
 import org.moeaframework.core.operator.real.SBX;
+import org.moeaframework.core.spi.OperatorFactory;
 import org.moeaframework.core.variable.Permutation;
 import org.moeaframework.problem.ExternalProblem;
 
@@ -87,11 +90,14 @@ public class FirewallIgniteIslandModel
 							new ChainedComparator(
 									new ParetoDominanceComparator(),
 									new CrowdingComparator())),
-					new GAVariation(new SBX(1.0, 25.0), new PM(0.1, 30.0)),
+					OperatorFactory.getInstance().getVariation(
+							null,
+							new Properties(),
+							problem),
 					new RandomInitialization(problem, 100));
 
 			// define the periodic action for performing migrations
-			PeriodicAction migrationAction = new PeriodicAction(algorithm, 1000, FrequencyType.STEPS) {
+			PeriodicAction migrationAction = new PeriodicAction(algorithm, 10000, FrequencyType.EVALUATIONS) {
 
 				@Override
 				public void doAction() {
@@ -120,7 +126,7 @@ public class FirewallIgniteIslandModel
 
 			};
 
-			while (migrationAction.getNumberOfEvaluations() < 40000) {
+			while (migrationAction.getNumberOfEvaluations() < 1000000) {
 				migrationAction.step();
 			}
 
@@ -134,29 +140,29 @@ public class FirewallIgniteIslandModel
 		try (Ignite ignite = Ignition.start("config/example-ignite.xml")) 
 		{
 			int port = 16800;
-			for (int i = 0; i < ISLANDS; i++) 
+			
+			//check if the executable exists
+			File file = new File("./examples/dtlz2_socket.exe");
+					
+			if (!file.exists()) 
 			{
-				
-				//check if the executable exists
-				File file = new File("./examples/dtlz2_socket.exe");
-						
-				if (!file.exists()) 
+				if (!SystemUtils.IS_OS_UNIX) 
 				{
-					if (!SystemUtils.IS_OS_UNIX) 
-					{
-						System.err.println("This example only works on POSIX-compliant systems; see the Makefile for details");
-						return;
-					}
-							
-					System.err.println("Please compile the executable by running make in the ./examples/ folder");
+					System.err.println("This example only works on POSIX-compliant systems; see the Makefile for details");
 					return;
 				}
-				
-				//run the executable and wait one second for the process to startup
-				Process pb = new ProcessBuilder(file.toString(), "" + (++port)).start();
-				Thread.sleep(1000);
-				
+						
+				System.err.println("Please compile the executable by running make in the ./examples/ folder");
+				return;
 			}
+			
+			// run one exe per island
+			for (int i = 0; i < ISLANDS; i++) 
+			{
+				Process pb = new ProcessBuilder(file.toString(), "" + (++port)).start();
+			}
+			
+			Thread.sleep(1000);
 			
 			Collection<IgniteCallable<NondominatedPopulation>> islands = new ArrayList<>();
 			
@@ -188,6 +194,9 @@ public class FirewallIgniteIslandModel
 				
 				combined.addAll(result);
 			}
+			
+			new Plot()
+			.add("Island Model", combined).show();
 		}
 	}
 
