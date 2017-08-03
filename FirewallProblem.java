dc83a67f2d9def19f4d8e06756c4251e856456bb
srdjan034 <snikolic.imi@gmail.com>
2017-08-03 16:32:36 +0200

    Dodat konstruktor za pokretanje exe-a.

--- FirewallProblem.java
+++ FirewallProblem.java
@@ -1,26 +1,21 @@
-package parallel.examples;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import org.moeaframework.core.Solution;
+import org.moeaframework.core.variable.EncodingUtils;
 import org.moeaframework.core.variable.Permutation;
 import org.moeaframework.problem.ExternalProblem;
 
 public class FirewallProblem extends ExternalProblem 
 {
-	private int n = 1000;
-	
-	public FirewallProblem() throws UnknownHostException, IOException
-	{
-		this("localhost", 16801);
-	}
-	
-	public FirewallProblem(String host, int port) throws IOException, UnknownHostException {
-		super(host, port);
-		// TODO Auto-generated constructor stub
+
+	public FirewallProblem(String[] command) throws IOException {
+		super(command);
 	}
 
+	private int n = 1000;
+	
 	@Override
 	public String getName() 
 	{
@@ -49,7 +44,8 @@ public class FirewallProblem extends ExternalProblem
 	public Solution newSolution() 
 	{
 		Solution solution = new Solution(1, 1);
-		solution.setVariable(0, new Permutation(n));
+		solution.setVariable(0, EncodingUtils.newPermutation(n));
+
 		return solution;
 	}
 
