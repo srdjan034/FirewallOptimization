819fe73044a8b40f2037fd81c302320fb7f7339f
srdjan034 <snikolic.imi@gmail.com>
2017-08-03 16:31:43 +0200

    Komunakiacija sa exe-om se odvija preko standardnog ulaza i izlaza.

--- FirewallIgniteIslandModel.java
+++ FirewallIgniteIslandModel.java
@@ -1,6 +1,5 @@
-package parallel.examples;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -14,6 +13,7 @@ import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.lang.IgniteBiPredicate;
 import org.apache.ignite.lang.IgniteCallable;
+import org.apache.ignite.lang.IgniteRunnable;
 import org.moeaframework.algorithm.NSGAII;
 import org.moeaframework.algorithm.PeriodicAction;
 import org.moeaframework.algorithm.PeriodicAction.FrequencyType;
@@ -32,6 +32,7 @@ import org.moeaframework.core.operator.real.PM;
 import org.moeaframework.core.operator.real.SBX;
 import org.moeaframework.core.spi.OperatorFactory;
 import org.moeaframework.core.variable.Permutation;
+import org.moeaframework.core.variable.RealVariable;
 import org.moeaframework.problem.ExternalProblem;
 
 import parallel.SynchronizedMersenneTwister;
@@ -41,24 +42,36 @@ import parallel.SynchronizedNondominatedSortingPopulation;
 public class FirewallIgniteIslandModel 
 {
 	
-	public static final int ISLANDS = 4;
+	public static int ISLANDS = 4;
 	
-	public static class IslandCallable implements IgniteCallable<NondominatedPopulation> {
+	public static class FirewallIslandCallable implements IgniteCallable<NondominatedPopulation> 
+	{
 		
 		private static final long serialVersionUID = -4006678228704675266L;
 
 		private final int islandIndex;
+		private String pathToExe = null;
+		private Problem problem;
+		private int migrationFrequency = 1000;
+		private int numberOfEvaluations = 10000;
 		
-		private final Problem problem;
-		
-		public IslandCallable(int islandIndex, Problem problem) {
+		public FirewallIslandCallable(int islandIndex, String pathToExe, 
+				int numberOfEvaluations, int migrationFrequency) throws Exception 
+		{
 			super();
 			this.islandIndex = islandIndex;
-			this.problem = problem;
+			this.pathToExe = pathToExe;
+			this.numberOfEvaluations = numberOfEvaluations;
+			this.migrationFrequency = migrationFrequency;
 		}
 		
 		@Override
-		public NondominatedPopulation call() throws Exception {
+		public NondominatedPopulation call() throws Exception 
+		{
+			String[] commands = new String[1];
+			commands[0] = pathToExe;
+			this.problem = new FirewallProblem(commands);
+			
 			PRNG.setRandom(SynchronizedMersenneTwister.getInstance());
 			
 			final Ignite ignite = Ignition.allGrids().get(0);
@@ -66,13 +79,15 @@ public class FirewallIgniteIslandModel
 			final List<Solution> incomingSolutions = new ArrayList<Solution>();
 			
 			// listen for incoming migrants
-			ignite.message().localListen("island" + islandIndex, new IgniteBiPredicate<UUID, Object>() {
-
+			ignite.message().localListen("island" + islandIndex, new IgniteBiPredicate<UUID, Object>() 
+			{
 				private static final long serialVersionUID = -2051222258821382910L;
 
 				@Override
-				public boolean apply(UUID id, Object solution) {
-					synchronized (incomingSolutions) {
+				public boolean apply(UUID id, Object solution) 
+				{
+					synchronized (incomingSolutions) 
+					{
 						incomingSolutions.add(((Solution)solution).copy());
 					}
 					
@@ -97,24 +112,30 @@ public class FirewallIgniteIslandModel
 					new RandomInitialization(problem, 100));
 
 			// define the periodic action for performing migrations
-			PeriodicAction migrationAction = new PeriodicAction(algorithm, 10000, FrequencyType.EVALUATIONS) {
+			PeriodicAction migrationAction = new PeriodicAction(algorithm, migrationFrequency, FrequencyType.EVALUATIONS) 
+			{
 
 				@Override
-				public void doAction() {
+				public void doAction() 
+				{
 					int targetIndex = PRNG.nextInt(ISLANDS-1);
 					
-					if (targetIndex >= islandIndex) {
+					if (targetIndex >= islandIndex) 
+					{
 						targetIndex += 1;
 					}
 
-					synchronized (population) {
+					synchronized (population) 
+					{
 						Solution emigrant = population.get(PRNG.nextInt(population.size()));
 						
 						ignite.message().send("island" + targetIndex,
 								emigrant.copy());
 						
-						synchronized (incomingSolutions) {
-							if (!incomingSolutions.isEmpty()) {
+						synchronized (incomingSolutions) 
+						{
+							if (!incomingSolutions.isEmpty()) 
+							{
 								int originalSize = population.size();
 								population.addAll(incomingSolutions);
 								population.truncate(originalSize);
@@ -126,51 +147,60 @@ public class FirewallIgniteIslandModel
 
 			};
 
-			while (migrationAction.getNumberOfEvaluations() < 1000000) {
+			while (migrationAction.getNumberOfEvaluations() < numberOfEvaluations) 
+			{
 				migrationAction.step();
 			}
+			
+			problem.close();
 
 			return migrationAction.getResult();
 		}
 		
 	}
 	
-	public static void main(String[] args) throws IOException, InterruptedException 
+	public static void main(String[] args) throws Exception 
 	{
-		try (Ignite ignite = Ignition.start("config/example-ignite.xml")) 
+		String pathToExe  = args[0];
+		String pathToIgniteConfFile  = args[1];
+		int numberOfEvaluations = Integer.parseInt(args[2]);
+		int migrationFrequency  = Integer.parseInt(args[3]);
+		ISLANDS = Integer.parseInt(args[4]);
+		
+		//check if the executable exists
+		File file = new File(pathToExe);
+		
+		if (!file.exists()) 
 		{
-			int port = 16800;
-			
-			//check if the executable exists
-			File file = new File("./examples/dtlz2_socket.exe");
-					
-			if (!file.exists()) 
+			if (!SystemUtils.IS_OS_UNIX) 
 			{
-				if (!SystemUtils.IS_OS_UNIX) 
-				{
-					System.err.println("This example only works on POSIX-compliant systems; see the Makefile for details");
-					return;
-				}
-						
-				System.err.println("Please compile the executable by running make in the ./examples/ folder");
+				System.err.println("This example only works on POSIX-compliant systems; see the Makefile for details");
 				return;
 			}
-			
-			// run one exe per island
-			for (int i = 0; i < ISLANDS; i++) 
-			{
-				Process pb = new ProcessBuilder(file.toString(), "" + (++port)).start();
-			}
-			
-			Thread.sleep(1000);
+					
+			System.err.println("Please compile the executable by running make in the ./examples/ folder");
+			return;
+		}
+		
+		//check if the conf file exists
+		file = new File(pathToIgniteConfFile);
+		
+		if (!file.exists()) 
+		{
+			System.err.println("Ignite conf file does not exists.");
+			return;
+		}
+				
+		Ignition.setClientMode(true);
+		
+		try (Ignite ignite = Ignition.start(pathToIgniteConfFile))
+		{
 			
 			Collection<IgniteCallable<NondominatedPopulation>> islands = new ArrayList<>();
 			
 			for (int i = 0; i < ISLANDS; i++) 
 			{
-				Problem problem = new FirewallProblem("localhost", port--);
-				islands.add(new IslandCallable(i+1, problem));
-
+				islands.add(new FirewallIslandCallable(i+1, pathToExe, numberOfEvaluations, migrationFrequency));
 			}
 		
 			Collection<NondominatedPopulation> results = ignite.compute().call(islands);
@@ -183,9 +213,10 @@ public class FirewallIgniteIslandModel
 				{
 					Permutation p = (Permutation)solution.getVariable(0);
 									
-					System.out.print("Fitness = "  + (-solution.getObjectives()[0]) + " - ");
+					System.out.print("\n\nFitness = "  + (-solution.getObjectives()[0]) + " - ");
 									
 					int[] solutionArray = p.toArray();
+					
 					for (int i = 0; i < solutionArray.length; i++) 
 					{
 						System.out.print(solutionArray[i] + ", ");
@@ -195,8 +226,7 @@ public class FirewallIgniteIslandModel
 				combined.addAll(result);
 			}
 			
-			new Plot()
-			.add("Island Model", combined).show();
+			ignite.cluster().stopNodes();
 		}
 	}
 
